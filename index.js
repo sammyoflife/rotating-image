212a1a927ac9c8372a6ff91b658f37eb533c8fa5
sammyoflife <samueltolu288@gmail.com>
2024-06-22 04:38:03 -0700

    rotating

---
+++ index.js
@@ -0,0 +1,28 @@
+const imagecontainerE1 = document.querySelector(".image-container");
+const prevE1 = document.getElementById("prev")
+const nextE1 = document.getElementById("next")
+
+let x = 0;
+let timer;
+
+function updateGallery() {
+    imagecontainerE1.style.transform = perspective(1000px) rotateY(${x}deg);
+    timer = setTimeout(() => {
+        x = x + 45;
+        updateGallery();
+ }, 3000 );
+}
+
+prevE1.addEventListener("click", () => {
+    x = x - 45;
+     clearTimeout(timer);
+     updateGallery();
+});
+
+nextE1.addEventListener("click", () => {
+    x = x + 45;
+    clearTimeout(timer);
+    updateGallery();
+})
+
+updateGallery();
